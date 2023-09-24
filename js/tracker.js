a4ae64dcb290e266a2f315333416f3b43185d2ff
stefan-milkovski99 <stefan.milkovski@generadevelopment.com>
2023-09-24 17:46:31 +0200

    Creating the javascript to the table

---
+++ js/tracker.js
@@ -0,0 +1,8 @@
+const expenseBtn = document.getElementById('expense-btn');
+const typeInput = document.getElementById('type-input');
+const nameInput = document.getElementById('name-input');
+const dateInput = document.getElementById('date-input');
+const amountInput = document.getElementById('amount-input');
+
+const tbodyInherit = document.querySelector('.inherit');
+
